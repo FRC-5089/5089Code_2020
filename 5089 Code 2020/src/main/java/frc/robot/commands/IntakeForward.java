e7e5f0cfdd1394a84d73ed5265fab0bac447973e
FRC-5089 <46613831+FRC-5089@users.noreply.github.com>
2020-02-12 15:32:46 -0800

    Add files via upload

---
+++ 5089 Code 2020/src/main/java/frc/robot/commands/IntakeForward.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.Constants;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+public class IntakeForward extends CommandBase {
+  /*
+   * Creates a new IntakeForward.
+   */
+
+
+  public VictorSPX Intake_Motor = new VictorSPX(Constants.Intake_10_ID);
+  public IntakeForward() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  Intake_Motor.set(ControlMode.PercentOutput,1);
+  
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    Intake_Motor.set(ControlMode.PercentOutput,0);
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

3c6ee77e1e8c7083ab9e35285196c917f1d19ec1
MA0610 <47308923+MA0610@users.noreply.github.com>
2020-03-04 16:23:06 -0800

    Update Robot.java

--- 5089 Code 2020/src/main/java/frc/robot/Robot.java
+++ 5089 Code 2020/src/main/java/frc/robot/Robot.java
@@ -122,6 +122,9 @@ public class Robot extends TimedRobot {
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
+	    
+	        CommandScheduler.getInstance().schedule(m_autonomousCommand);
+
     }
   }
 
@@ -140,6 +143,10 @@ public class Robot extends TimedRobot {
     // this line or comment it out.
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
+	    
+	    
+	        CommandScheduler.getInstance().cancel(m_autonomousCommand);
+
     }
   }
 

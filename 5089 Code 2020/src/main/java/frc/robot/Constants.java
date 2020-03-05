e568272d313d788ee900f99cdb920e3163615071
MA0610 <47308923+MA0610@users.noreply.github.com>
2020-03-05 15:39:09 -0800

    Update Constants.java

--- 5089 Code 2020/src/main/java/frc/robot/Constants.java
+++ 5089 Code 2020/src/main/java/frc/robot/Constants.java
@@ -15,58 +15,45 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
-	
-	//Drive
-	public static final int FLMOTOR_1_ID = 0;
-	public static final int MLMOTOR_2_ID = 1;
-	public static final int BLMOTOR_3_ID = 2;
-	public static final int FRMOTOR_4_ID = 3;
- 	public static final int MRMOTOR_5_ID = 4;
-	public static final int BRMOTOR_6_ID = 5;
-	
+public final class Constants 
+{
+    public static int Driver = 0, Co_Driver = 1;
+    public static int Left_Y = 1, Right_Y = 3;
+
+    
+
+    public static int Controller2 = 1;
+
+    //Drive
+    public static int Drive1 = 0, Drive2 = 1, Drive3 = 2, Drive4 = 3, Drive5 = 4, Drive6 = 5;
 
     //Intake
     public static int Intake = 6;
 
-	//Conveyor
-	public static final int Con_7_ID = 7;
-	
-	//Shooter
-	public static final int Shooter_8_ID = 8;
-	public static final int Shooter_9_ID = 9;
+    //Conveyor
+    public static int Converyor = 7;
 
-    
+    //Shooter
+    public static int Shooter1 = 8, Shooter2 = 9;
+
     //Lift
     public static int Lift1 = 10, Lift2 = 11;
-	
+
     //Arm
     public static int Arm1 = 12;
 
     //Wheel of Fortune
-    public static int EncoderM = 13;
+    public static int Wheel_of_Fortune = 13;
+
+    //Pully Retract
+    public static int Pully1 = 14;
 
     //Compressor
     public static int Compressor = 0;
 
     //Solenoid
-    public static int Extend = 5, Retract = 2;
+    public static int Extend = 5, Retract = 2, Fast = 1, Slow = 6;
     public static int Lift = 1, Lower = 0;
     
-	
-	
-
-	public static final int Drive_Controller_Right_Y = 3;
-	public static final int Drive_Controller_Left_Y = 1;
-	
-	
-	public static final int Joystick_Driver = 0;
-	public static final int Joystick_CoDriver = 1;
-
-	public static final int Joystick_buttonB = 1;
-	public static final int Joystick_buttonG = 2;
-	public static final int Joystick_buttonR = 3;
-	public static final int Joystick_buttonY = 4;
-
-  
+    
 }
